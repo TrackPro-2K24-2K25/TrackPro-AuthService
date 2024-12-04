4eba0da1dfa5e96a98465bd3f54d5b32931b0aa8
ibtihal boukhanchouch <105915840+itzibtihal@users.noreply.github.com>
2024-12-04 16:51:34 +0100

    create auth service project and connect it with supabase

---
+++ src/main/java/org/youcode/trackproauthservice/TrackProAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package org.youcode.trackproauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackProAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackProAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/youcode/trackproauthservice/TrackProAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.youcode.trackproauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackProAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
